c4e16aa58e05896c78f998b070cf08b36f072763
Karishma Sahoo <karishmasahoo30@gmail.com>
2020-05-20 15:57:15 +0530

    Update invisible cloak.py

--- invisible cloak.py
+++ invisible cloak.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 cap=cv2.VideoCapture(0)
 background=0
+#for capturing the background image
 for i in range(30):
     ret,background=cap.read()
 background=np.flip(background,axis=1)
@@ -11,17 +12,11 @@ while(cap.isOpened()):
     ret,img=cap.read()
     img=np.flip(img,axis=1)
     hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    #range for lower red
-    lower_red=np.array([94,80,20])
-    upper_red=np.array([126,255,255])
-    mask1=cv2.inRange(hsv, lower_red, upper_red)
+    #range for blue color
+    lower_blue=np.array([94,80,20])
+    upper_blue=np.array([126,255,255])
+    mask1=cv2.inRange(hsv, lower_blue, upper_blue)
     #cv2.imshow("mask1",mask1)
-    #range for upper red
-    #lower_red=np.array([110,50,50])
-    #upper_red=np.array([130,255,255])
-    #mask2=cv2.inRange(hsv, lower_red, upper_red)
-    #cv2.imshow("mask2",mask2)
-    #qmask1=mask1+mask2
     mask1=cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
     mask1=cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))
     #cv2.imshow("mask1",mask1)
